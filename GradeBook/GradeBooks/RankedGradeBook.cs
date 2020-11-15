06eadf03196cd783f3412b536a2f8e9f80df4427
JanineA <Janine.Aguilar@asb.co.nz>
2020-11-16 12:28:11 +1300

    GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,37 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            else
+            {
+                int studentsRank = Convert.ToInt32(Students.Count / 5) -1;
+                List<double> grades = new List<double>();
+
+                foreach(var student in Students)
+                {
+                    grades.Add(Convert.ToDouble(student.Grades));
+                }
+
+                grades.Sort();
+
+                if (grades.IndexOf(averageGrade) <= studentsRank)
+                    return 'A';
+                else if (grades.IndexOf(averageGrade) <= (studentsRank * 2))
+                    return 'B';
+                else if (grades.IndexOf(averageGrade) <= (studentsRank * 3))
+                    return 'C';
+                else
+                    return 'D';
+            }
+              
+            return 'F';
+            
+            
+
+        }
     }
 }
